eb96139689e72a57f383ed8f78fb2d69c00fdcda
Scorpionno <jigoku27772@gmail.com>
2024-02-08 22:10:44 +0100

    kolorowytest

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+const startBtn = document.querySelector('[data-start'); // błąd!:[start-stop]
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = 0;
+
+function startColorSwitching() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+    }, 1000);
+}
+
+function stopColorSwitching() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+}
+
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitching);

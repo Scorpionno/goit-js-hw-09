6f1466eee809eca184a8e05ee1e2348c8db9f326
Scorpionno <148999859+Scorpionno@users.noreply.github.com>
2024-02-08 21:39:20 +0000

    Deploying to gh-pages from @ Scorpionno/goit-js-hw-09@acd7a86c7453ba6b28b0c06bda10612223897ce7 🚀

---
+++ 01-color-switcher.47e6546c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.47e6546c.js.map
